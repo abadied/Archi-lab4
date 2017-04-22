ca9323e07ef40e15260b8bcafa9f45304a5daaa9
abadied <abadi.eden@gmail.com>
2017-04-22 23:20:13 +0300

    2b

--- task2b/flame2.c
+++ task2b/flame2.c
@@ -14,7 +14,7 @@ typedef struct ent{
     int offset;
     short len;
     char buf[1];
-    char type[1];
+    
 }ent;
 
 int main (int argc , char* argv[], char* envp[])
@@ -27,7 +27,7 @@ int main (int argc , char* argv[], char* envp[])
   int fd,j;
   int count = 0;
   int i = 0;
-  char* type_arg;
+  char type_arg;
   int suffix = 0;
   ent* entd = buffer;
   for(j=1;j<argc;j++){
@@ -35,7 +35,7 @@ int main (int argc , char* argv[], char* envp[])
           debug = 1;
       }
       else if (strcmp(argv[j],"-s")==0){
-          type_arg = argv[j + 1];
+          type_arg = argv[j + 1][0];
           suffix = 1;
       }
   }
@@ -47,18 +47,17 @@ int main (int argc , char* argv[], char* envp[])
   
   while(count>28){
     entd = buffer + i ;
-    if(!suffix || entd->type == type_arg){
+    int length = 0;
+    while(entd->buf[length]!=0)
+        length = length + 1;
+
+    if(!suffix || entd->buf[length-1]==type_arg){
         system_call(SYS_WRITE,output,"file name:",11);
         system_call(SYS_WRITE,output,entd->buf,10);
         system_call(SYS_WRITE,output,"\n",1);
     }
-    /*
-    system_call(SYS_WRITE,output,"file type:",11);
-    
-    system_call(SYS_WRITE,output,entd->type,1);
-    system_call(SYS_WRITE,output,"\n",1);
-    */
-    if(debug &&(!suffix|| entd->type == type_arg)){
+
+    if(debug &&(!suffix||entd->buf[length-1]==type_arg)){
         system_call(SYS_WRITE,output,"file length:",13);
         system_call(SYS_WRITE,output,itoa(entd->len) ,2);
         system_call(SYS_WRITE,output,"\n",1);
